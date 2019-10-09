d39426fe7557b28ed3b069aecb8a6ceec1b7945c
Guillaume <31861158+GuillaumeD42@users.noreply.github.com>
2019-10-08 20:35:46 -0400

    Update Tree_Code.py

--- Tree_Code.py
+++ Tree_Code.py
@@ -22,6 +22,7 @@ Federer_copy = Federer_raw[Federer_raw.opponent_rank != '-']
 Federer = Federer_copy.copy()
 
 # Filling in the missing values for these columns with the median of each ones
+
 Federer.player_first_serve_points_won_percentage.fillna(Federer.player_first_serve_points_won_percentage.median(), inplace=True)
 Federer.player_first_serve_percentage.fillna(Federer.player_first_serve_percentage.median(), inplace=True)
 Federer.games_won.fillna(Federer.games_won.median(), inplace=True)
@@ -38,15 +39,18 @@ Federer['player_aces'] = Federer['player_aces'].astype(int)
 Federer['player_first_serve_points_won_percentage'] = Federer['player_first_serve_points_won_percentage'].astype(int)
 
 # Prediction of the mean of the entire dataset
+
 Federer['prediction'] = Federer.Victory.mean()
 Federer.head()
 
 # RMSE for those predictions
+
 from sklearn import metrics
 import numpy as np
 np.sqrt(metrics.mean_squared_error(Federer.Victory, Federer.prediction))
 
 # RMSE for a given split of first serve points won (fspw)
+
 def firstservepc_split(player_first_serve_points_won_percentage):
     lower_firstserve_win = Federer[Federer.player_first_serve_points_won_percentage < player_first_serve_points_won_percentage].Victory.mean()
     higher_firstserve_win = Federer[Federer.player_first_serve_points_won_percentage >= player_first_serve_points_won_percentage].Victory.mean()
@@ -54,33 +58,39 @@ def firstservepc_split(player_first_serve_points_won_percentage):
     return np.sqrt(metrics.mean_squared_error(Federer.player_first_serve_points_won_percentage, Federer.prediction))
 
 # calculate RMSE for tree which splits on fspw < 70
+
 print("RMSE :", firstservepc_split(72))
 Federer
 
 # check all possible mileage fspw
+
 firstserve_range = range(Federer.player_first_serve_points_won_percentage.min(), Federer.player_first_serve_points_won_percentage.max(), 1)
 RMSE = [firstservepc_split(player_first_serve_points_won_percentage) for player_first_serve_points_won_percentage in firstserve_range]
 
 # allow plots to appear in the notebook
+
 get_ipython().magic('matplotlib inline')
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = (6, 4)
 plt.rcParams['font.size'] = 14
 
 # plot fswp cutpoint (x-axis) versus RMSE (y-axis)
+
 plt.plot(firstserve_range, RMSE)
 plt.xlabel('First Serve Cutpoint')
 plt.ylabel('RMSE (lower is better)')
 
 
-## Building a regression tree in scikit-learn
+# Building the regression tree in scikit-learn
 
 # define X and y
+
 feature_cols = ['player_ranking', 'player_first_serve_percentage', 'opponent_rank', 'match_duration', 'player_first_serve_points_won_percentage', 'player_aces']
 X = Federer[feature_cols]
 y = Federer.Victory
 
 # instantiate a DecisionTreeRegressor (with random_state=1)
+
 from sklearn.tree import DecisionTreeRegressor
 treereg = DecisionTreeRegressor(random_state=1)
 treereg
@@ -91,41 +101,49 @@ scores = cross_val_score(treereg, X, y, cv=14, scoring='mean_squared_error')
 np.mean(np.sqrt(-scores))
 
 
-## Tuning a regression tree
+## Tuning the regression tree
 
 # try different values one-by-one
+
 treereg = DecisionTreeRegressor(max_depth=1, random_state=1)
 scores = cross_val_score(treereg, X, y, cv=14, scoring='mean_squared_error')
 np.mean(np.sqrt(-scores))
 
 # list of values to try
+
 max_depth_range = range(1, 10)
 
 # list to store the average RMSE for each value of max_depth
+
 RMSE_scores = []
 
 # use LOOCV with each value of max_depth
+
 for depth in max_depth_range:
     treereg = DecisionTreeRegressor(max_depth=depth, random_state=1)
     MSE_scores = cross_val_score(treereg, X, y, cv=14, scoring='mean_squared_error')
     RMSE_scores.append(np.mean(np.sqrt(-MSE_scores)))
 
 # plot max_depth (x-axis) versus RMSE (y-axis)
+
 plt.plot(max_depth_range, RMSE_scores)
 plt.xlabel('max_depth')
 plt.ylabel('RMSE (lower is better)')
 
 # max_depth=3 was best, so fit a tree using that parameter
+
 treereg = DecisionTreeRegressor(max_depth=3, random_state=1)
 treereg.fit(X, y)
 
 # "Gini importance" of each feature: the (normalized) total reduction of error brought by that feature
+
 pd.DataFrame({'feature':feature_cols, 'importance':treereg.feature_importances_})
 
 
-## Creating a tree diagram
+## Creating the tree diagram
 
 # create a Graphviz file
+
 from sklearn.tree import export_graphviz
 export_graphviz(treereg, out_file='Tree_Federer_VictoryTest.dot', feature_names=feature_cols)
 
@@ -136,9 +154,11 @@ export_graphviz(treereg, out_file='Tree_Federer_VictoryTest.dot', feature_names=
 ## Making predictions for the testing data
 
 # read the testing data
+
 Federer
 
 # Fitted model to make predictions on testing data
+
 X_Federer = Federer[feature_cols]
 y_Federer = Federer.Victory
 y_pred = treereg.predict(X_Federer)
@@ -146,6 +166,7 @@ y_pred
 
 
 # calculate RMSE
+
 np.sqrt(metrics.mean_squared_error(y_Federer, y_pred))
 y_Federer = [60, 70, 80]
 y_pred = [0, 0, 0]
